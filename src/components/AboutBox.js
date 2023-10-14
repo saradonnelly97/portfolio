faa43c74745d2189f50c199fe830b0fc472e11f1
saradonnelly97 <sara.donnelly8@gmail.com>
2023-10-14 14:11:13 -0400

    styling update

--- src/components/AboutBox.js
+++ src/components/AboutBox.js
@@ -5,8 +5,8 @@ function AboutBox() {
     <div className="about__boxes grid">
         <div className="about__box">
             <div>
-                <h3 className="about__title">32</h3>
-                <span className="about__subtitle">Projects Completed</span>
+                <h3 className="about__title">80</h3>
+                <span className="about__subtitle">GitHub Projects Completed</span>
             </div>
         </div>
 
@@ -19,8 +19,8 @@ function AboutBox() {
 
         <div className="about__box">
             <div>
-                <h3 className="about__title">1</h3>
-                <span className="about__subtitle">Satisfied Clients</span>
+                <h3 className="about__title">146</h3>
+                <span className="about__subtitle">Artworks Executed</span>
             </div>
         </div>
 
